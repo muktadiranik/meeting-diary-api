869922808cc26545ae8ff359f180014f01078c11
muktadiranik <muktadiranik@gmail.com>
2023-07-30 14:47:58 +0600

    urls.py modified

--- backend/urls.py
+++ backend/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('accounts/', include('allauth.urls')),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/auth/', include('dj_rest_auth.urls')),
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/', include('meetings.urls')),
-    path('search/', SearchAPIView.as_view()),
+    path('api/search/', SearchAPIView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

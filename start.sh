ed5db2f368567cec08800d364af89e889ee56b2c
muktadiranik <muktadiranik@gmail.com>
2023-09-30 11:20:05 +0600

    shell scripts modified

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 # !/bin/bash
 
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the requirements
+pip install -r requirements.txt
+
 # Start the Celery worker
 celery -A backend worker -l info &
 
